e1da632aa178d34238dffd793b7e62b6f726b408
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-21 18:44:31 -0400

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ LABEL org.opencontainers.image.version="0.0.1"
 LABEL com.harbormaster.blueprint="Golang"
 LABEL com.harbormaster.model="Banking Industry Domain Model"
 LABEL com.harbormaster.generated="2026-09-21"
-#LABEL com.harbormaster.certification="e73efaf9-920a-4414-b284-c44f632a14cf"
+#LABEL com.harbormaster.certification="7c53e405-ee3d-4abc-a3d7-e6622b7b8df5"
 
 WORKDIR /app
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
       CreatedBy = "Harbormaster"
       Blueprint = "Golang"
       DomainModel = "Banking Industry Domain Model"
-      CertificationId = "e73efaf9-920a-4414-b284-c44f632a14cf"
+      CertificationId = "7c53e405-ee3d-4abc-a3d7-e6622b7b8df5"
     }
   }
 }
@@ -39,7 +39,7 @@ resource "local_file" "private_key_pem" {
 }
 
 resource "aws_key_pair" "generated" {
-  key_name   = "pjsk-sshtest-0.9392512749902828"
+  key_name   = "pjsk-sshtest-0.6395282639448518"
   public_key = tls_private_key.generated.public_key_openssh
 
   lifecycle {
